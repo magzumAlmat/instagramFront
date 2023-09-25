3ae055e518de9fac7c44393239dd1cde129568d8
magzumAlmat <almat.magzum@gmail.com>
2023-09-25 11:05:35 +0600

    signupForm

---
+++ src/app/signup/page.js
@@ -0,0 +1,14 @@
+'use client'
+import Header from '@/components/header'
+import UserLogin from '@/components/auth/user'
+import UserSignUp from '@/components/signup'
+
+export default function Login() {
+  return (
+    <main>
+      <Header/>
+      
+      <UserSignUp/>
+    </main>
+  )
+}
--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -2,23 +2,26 @@
 import Image from 'next/image'
 import insta from '@/app/images/insta.png'
 import Link from 'next/link'
- export default function UserLogin() {
+ export default function UserSignUp() {
     return (
         <>
             <section className="login-page">
                 <div className="card">
                   <Image src={insta}/>
+                  <p className='pheader'>Зарегистрируйтесь, чтобы смотреть фото и видео ваших друзей.</p>
                     <form action="">
+                        <input type="text" className="input"/>
+                        <input type="password" className="input"/>
                         <input type="text" className="input"/>
                         <input type="password" className="input"/>
                       
                         <button className="button button-primary">
-                            Войти
+                            Регистрация
                         </button>
                     </form>
                 </div>
                 <div className="card">
-                  <p>У вас ещё нет аккаунта? <Link href="signup/">Зарегистрироваться</Link></p>
+                  <p>Есть аккаунт? <Link href="/login">Вход</Link></p>
                 </div>
 
              
