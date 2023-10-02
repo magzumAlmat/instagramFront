3923ae41fd1812e052687f19b4d02a92f2a932aa
Billionare <billionare@mac.local>
2023-10-02 10:34:08 +0600

    second style corrected

--- src/components/recposts/recpost/index.js
+++ src/components/recposts/recpost/index.js
@@ -1,15 +1,15 @@
-'use client'
+'use client' 
 import Image from 'next/image'
 import notificationLogo from '@/app/images/notification-small.svg';
 import likeLogo from '@/app/images/heart-3510.svg';
 import commentLogo from '@/app/images/instagram-comment-13415.svg';
 import shareLogo from '@/app/images/instagram-share-13421.svg';
 import saveLogo from '@/app/images/save-instagram-black-lineal-18315.svg';
-export default function RecommendedPost({post, users, myposts}){
-  
-    const host='http://157.245.193.184:3002'
+export default function RecommendedPost({post, users, myposts}) {
+
+    const host = 'http://157.245.193.184:3002'
     const data3 = []
-    users.map(i=>{
+    users.map(i => {
         data3.push(i.id)
     })
     let authorPost = '';
@@ -18,43 +18,40 @@ export default function RecommendedPost({post, users, myposts}){
     console.log(data3)
 
 
-
-
-    const ram=post.likes
+    const ram = post.likes
     const data = []
-    ram.map(i=>{
-           data.push(i) 
+    ram.map(i => {
+        data.push(i)
     })
 
-    console.log('post ',post)
+    console.log('post ', post)
     // console.log('comments ',post.commentaries)
-    const ramComments=post.commentaries
+    const ramComments = post.commentaries
     const data2 = []
-    ramComments.map(i=>{
-           data2.push(i);
-           users.map(u => {
-               if (u.id == i.authorId) {
-                   authorComment = u.username
-               }
-           })
+    ramComments.map(i => {
+        data2.push(i);
+        users.map(u => {
+            if (u.id == i.authorId) {
+                authorComment = u.username
+            }
+        })
     })
-
 
 
     const data4 = []
-    myposts.map(i=>{
+    myposts.map(i => {
         users.map(u => {
             if (u.id == i.creatorId) {
                 authorPost = u.username
                 console.log('check resolve', u.username)
             }
-            console.log('this is user from map' ,u)
+            console.log('this is user from map', u)
         })
         data4.push(i)
-        console.log( 'this is post', i)
+        console.log('this is post', i)
     })
     let comment = []
-  
+
 
     // console.log('data2',data2)
     const mapComment = data2.map(i => {
@@ -64,64 +61,85 @@ export default function RecommendedPost({post, users, myposts}){
     // console.log('Likes= ',typeof(numberOfLikes.likes))
 
 
-
     return (
-    <div className="post">
-        <h6>{authorPost}</h6>
-        <Image
-            src={`${host}/${post.mediaLinks}`}
-            alt="some alt"
-            width={500}
-            height={500}
-        />
-
-        {/* <div className=''>
+        <div className="post">
+            <div className='gap'>
+            <span >{authorPost}</span>
+            </div>
+            
+            <Image src={
+                    `${host}/${
+                        post.mediaLinks
+                    }`
+                }
+                alt="some alt"
+                width={500}
+                height={500}/> {/* <div className=''>
             <a className=""> <Image src={notificationLogo} alt='some alt2'  className='like'   /></a>
             <a className=""> <Image src={notificationLogo} alt='some alt2'  className='comment'/></a>
             <a className=""> <Image src={notificationLogo} alt='some alt2'  className='send'     /></a>
             <a className="saveLink"> <Image src={notificationLogo} alt='some alt2'  className='save'   id="raw"  /></a>
         </div> */}
 
-        < div id = "flex-container" > <div className="flex-item1" id="flex">
-            <a className="">
-                <Image src={likeLogo}
-                    alt='some alt2'
-                    className='like'/></a>
-            <a className="">
-                <Image src={commentLogo}
-                    alt='some alt2'
-                    className='comment'/></a>
-            <a className="">
-                <Image src={shareLogo}
-                    alt='some alt2'
-                    className='send'/></a>
+            <div id="flex-container">
+                <div className="flex-item1" id="flex">
+                    <a className="">
+                        <Image src={likeLogo}
+                            alt='some alt2'
+                            className='like'/></a>
+                    <a className="">
+                        <Image src={commentLogo}
+                            alt='some alt2'
+                            className='comment'/></a>
+                    <a className="">
+                        <Image src={shareLogo}
+                            alt='some alt2'
+                            className='send'/></a>
+                </div>
+                <div className="raw-item1" id="raw">
+                    <a>
+                        <Image src={saveLogo}
+                            alt='some alt2'
+                            className='save'
+                            id="raw"/></a>
+                </div>
+            </div>
+        <div className='gap'>    
+            <div className="counOftLikes" >
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                <span> {
+                    data.length
+                }{' '}
+                    отметок "Нравится"</span>
+            </div>
+
+            <div className='authorname'>
+            <span>{authorPost}
+            {' '}{
+                post.description
+            }</span>
+            
+            </div>
+
+            <div className="comments">
+                {comment.map((key) => (
+                    // eslint-disable-next-line react/jsx-key <div>
+                     <div>
+                     <span>
+                    {authorComment} {' '}{key}
+                 
+                  
+
+                    </span>
+                    <h6 className='comment-text'> </h6>
+                    </div>
+
+
+                ))
+            } </div>
         </div>
-        <div className="raw-item1" id="raw">
-            <a>
-                <Image src={saveLogo}
-                    alt='some alt2'
-                    className='save'
-                    id="raw"/></a>
-        </div></div><div className="counOftLikes">
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <h6> {data.length} отметок "Нравится"</h6></div>
-        <h6>{authorPost} {post.description}</h6>
+        </div>
+    )
 
-
-
-        <div className="comments">
-            {comment.map((key) => (
-                // eslint-disable-next-line react/jsx-key
-                <h6>
-                    {authorComment} {key}
-                </h6>
-                
-            ))}
-        </div> 
-    </div>
-        )
-
-    }
-
-
+}
 
