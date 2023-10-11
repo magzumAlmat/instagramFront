cb86894b4e6c81b09eb8ce03c667f09daf799687
Billionare <billionare@mac.local>
2023-10-11 17:30:07 +0600

    add detail page to profile

--- src/components/profile/posts/post/index.js
+++ src/components/profile/posts/post/index.js
@@ -3,13 +3,44 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Modal from '@/components/createpost'
 import ModalPost from './modalpost'
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import { getAllUsersAction } from '@/store/slices/getUsersPostsSlice'
+import { useDispatch,useSelector } from 'react-redux'
+import axios from 'axios'
 export default function Post({post,currentUser}){
-
- 
-
     
+    // const allUsers=useSelector(((state) => state.userposts.allUsers))
+    const [allUsers, setUsers] = useState([]);
+    console.log("ALLL USERS",allUsers)
+    const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhbG1hdC5tYWd6dW0xMjM0QGdtYWlsLmNvbSIsImZ1bGxfbmFtZSI6bnVsbCwicGhvbmUiOm51bGwsImlhdCI6MTY5NTY5ODE5NSwiZXhwIjoxNzI3MjM0MTk1fQ.r4M018A6NHYIV6tMAcaQOQowb3IhmHZ5u9VnSzRBEik'
+    // useEffect(() => {
+    //     dispatch(getAllUsersAction());
+    //   }, [allUsers]);
+    
+    useEffect(() => {
+        
+        const fetchMyPosts = async () => {
+            try {
+                const users = await axios.get('http://157.245.193.184:3002/api/getallusers', {
+                        headers: {
+                            'Authorization': `Bearer ${authToken}`
+                        }
+                })
+
+                setUsers(users.data)
+                
+              
+            } catch (error) {
+                
+                console.error('Error fetching posts:', error);
+            }
+            
+        };
+
+        
+        fetchMyPosts();
+    }, []);
+
     // console.log('3 Post=',post.mediaLinks)
     const [clickedPost, setClickedPost] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -44,7 +75,7 @@ export default function Post({post,currentUser}){
             />
         <Modal isOpen={isModalOpen} onClose={closeModal}>
             {console.log('id post clicked post', clickedPost)}
-            <ModalPost post={clickedPost}/>
+            <ModalPost post={clickedPost} users={allUsers} />
         </Modal>
      
      </a>
--- src/components/profile/posts/post/modalpost/index.js
+++ src/components/profile/posts/post/modalpost/index.js
@@ -11,147 +11,259 @@ import emoji from '@/app/images/smartphone.webp';
 
 import { authSlice,authorize,logout } from '@/store/slices/authSlice'
 import { useSelector, useDispatch } from 'react-redux'
-export default function ModalPost ({post}) {
-    console.log('post from modal post', post)
+import axios from "axios";
+import { addPostCommentAction } from "@/store/slices/getUsersPostsSlice";
+
+export default function ModalPost ({post,users,like}) {
+    const dispatch = useDispatch()
+    console.log('111post from modal post', post)
+    console.log('11users from modal post', users)
   
+    const host = 'http://157.245.193.184:3002';
 
+    let comments=post.commentaries
 
+    const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhbG1hdC5tYWd6dW0xMjNAZ21haWwuY29tIiwiZnVsbF9uYW1lIjpudWxsLCJwaG9uZSI6bnVsbCwiaWF0IjoxNjk0NTg2NjczLCJleHAiOjE3MjYxMjI2NzN9.KTEqxyqQJ5avV6maDzAccZknj16_9m3g2NEOlwUch44';
+    
+    const [newComment, setNewComment] = useState(""); // State to hold the new comment
 
+    const handleCommentChange = (event) => {
+      setNewComment(event.target.value); // Update the new comment state
+    };
+  
+    const handlePublishClick = async() => {
+      // Handle the "Опубликовать" click event
+      console.log("Publishing comment: ", newComment);
+      await dispatch(addPostCommentAction(newComment,post))
+      alert('comment uploaded')
+
+      
+
+    }
+    let mainImage=''
+    
+    post.mediaLinks.map((item,index) => {
+        console.log('img=',item)
+    mainImage=`${host}/${item}`
+    })
+
+    let nodata=comments
+
+    console.log('c',comments,comment.length)
+
+    if (comments===undefined){
+        nodata=[{0:'NODATA'}]
+        console.log('COMMENT HAVE NO DATA',comments,nodata)
+       }
 
     return (
-            <div className="modal-post">
-               
+        
+        <div className="modal">
+
+        {console.log('post.commentaries.====',post.commentaries)}
+      
+        
+
+       
+             {  nodata.map((item, index) => (
+                
+                //  console.log(' 1 item from post',post),
+                
+                <div key={item.id} className="modal-post">
                     <div className='modal-post-left'>
-                        asd
-                        <Image src={img} alt="alt" />
+                        
+                        <Image  src={mainImage} alt="some aasdaasdlt"
+                            width={500}
+                            height={500}/>
                     </div>
+                 
+                    
                     <div className='modal-post-right'>
                         <header className='modal-post-right-header'>
+                            
                             <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
+                                {/* <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt="asdad"/> */}
                             </div>
+                            <br />
                             <div className='modal-post-right-header-username'>
-                                <a href='#'>Username</a>
-                                <a href='#'>city</a>
+                            
+                            {users.map((item3, index) => {
+                            if(item3.id==post.creatorId){
+                                return(
+                                <div>
+                                {item3.username}
+                                </div>
+                                )         
+                                                                    }
+                                                  }
+                        )}
+                                {/* <a href='#'>city</a> */}
                             </div>
                         </header>
                         <div className="modal-post-right-body">
-                            <div className='flex flex-ai-c flex-rw'>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                                <span>comment</span>
-                            </div>
-                            <time>time</time>
+                        {users.map((item3, index) => {
+                            if(item3.id==post.creatorId){
+                                return(
+                                <div>
+                                 {item3.username} {post.description}
+                                </div>
+                                )         
+                                                                    }
+                                                  }
+                        )}
+                        <br />
+                        <br />
+                            <div className=''>
+                            {/* {comment.length == 0  ? (
+                            
+                           
+                            
+                            ):(
+                                {comment=nodata}
+                            )} */}
 
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
+                                {nodata.map((i) => (
+                                     users.map((item3, index) => {
+                                     if(item3.id==i.authorId){
+                                        return(<div key={i.id}>{item3.username}   {i.commentary}</div>)
+                                        // console.log('output i=',item3.username,i.commentary),
+                                        
+                                     }
+                                     })
+                                    ))}
+
+                                    {/* <p>----------------------------------------------</p> */}
+                                {/* <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt="asdads"/> */}
+                                {/* {item.commentary.map((item2)=>(<span key={item2.id}>{item2}</span>))} */}
+                                {/* {console.log(' render coments= ',item.commentary)} */}
+                                
+
+                               {/* {console.log('cc=====',comments)}
+                                    
+                               
+                                  {comments.map((i, index) => (
+                                    users.map((item3, index) => {
+                                        if(i.authorId===item3.id){
+                                            <div key={item.id} className="modal-post">
+                                                
+                                               asdada {item3.username}   {i.commentary}
+                                            </div>
+                                        }})
+
+                                ))
+                                } */}
+
+                                {/* {cc && cc.length > 0  ? (
+                                        comments.map((i, index) => (
+                                            users.map((item3, index) => {
+                                                if(item3.id==i.authorId){
+                                                    <div key={item.id} className="modal-post">
+                                                    
+                                                        {item3.username}   {i.commentary}
+                                                    </div>
+                                                }})
+
+                                        ))
+                                    ) : (
+                                        <p>No comments available.</p>
+                                    )} */}
+                               
+                               
+                                {/* {comments.map((i) => (
+                                     users.map((item3, index) => {
+                                     if(item3.id==i.authorId){
+                                        return(<div key={i.id}>{item3.username}   {i.commentary}</div>)
+                                        // console.log('output i=',item3.username,i.commentary),
+                                        
+                                     }
+                                     })
+                                    ))} */}
+                               
+                              
                             </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
-                            <div>
-                                <Image src={smallProfile} style={{"height":'32px', "width": '32px'}} alt=""/>
-                            </div>
-                            <div className='modal-post-right-header-username'>
-                                <span>comment</span>
-                                <time>time</time>
-                            </div>
+                            <br />
+                            {/* <time>{item.createdAt}</time> */}
+
+                           
                         </div>
-                        <div className="modal-post-section">
+                        {/* <div className="modal-post-section">
                             <div className='modal-post-section-start'>
                                 <button>
-                                    <Image src={heart}/>
+                                    <Image src={heart} alt="asdads"/>
                                 </button>
                                 <button>
-                                    <Image src={comment}/>
+                                    <Image src={comment} alt="asdads"/>
                                 </button>
                                 <button>
-                                    <Image src={share}/>
+                                    <Image src={share} alt="asdads"/>
                                 </button>
                             </div>
                             <div className='modal-post-section-end'>
                                 <button>
-                                    <Image src={save}/>
+                                    <Image src={save} alt="asdads"/>
                                 </button>
                             </div>
-                        </div>
-                        <div className="modal-post-liked p1">
+                        </div> */}
+                        <div className="modal-post-liked ">
                             <div>
-                                <Image src={smallProfile} style={{'height': '16px', 'width': '16px'}}></Image>
-                                <Image src={smallProfile} style={{'height': '16px', 'width': '16px'}}></Image>
-                                <Image src={smallProfile} style={{'height': '16px', 'width': '16px'}}></Image>
+                                <Image src={smallProfile} style={{'height': '16px', 'width': '16px','margin': '20px'}} alt="asdads"/>
                             </div>
                             <div>
-                                Нравится username
-                            </div>
+                            {/* {post.likes.map((i) => (
+                                     users.map((item3, index) => {
+                                     if(item3.id==i.authorId){
+                                        return(   <div >{like} Нравится {item3.username}</div>)
+                                     }
+                                    
+                                     }
+                            )
+                                )    )
+                            } */}
+                            
+                            {/* <div >{post.likes.map((i) => {
+                                let sum=0
+                                sum=sum+i.length
+                              
+                            })}
+                                
+                               
+                                {like.length > 0 && (
+                                    <div>
+                                        {like[0]} отметок "Нравится"
+                                    </div>
+                                    )}
+                                </div> */}
+                                </div>
                         </div>
-                        <div className='p1'>
-                            timestamp
+                        <div className='section'>
+                        <time>{item.createdAt}</time>
                         </div>
-                        <section className='section'>
+                        <section className='section '>
                             <div className='modal-post-comment'>
-                                <div>
-                                    <Image style={{'width': '32px', 'height': '32px'}} src={emoji}/>
+                                <div className="section">
+                                <svg aria-label="Смайлик" class="x1lliihq x1n2onr6" color="rgb(0, 0, 0)" fill="rgb(0, 0, 0)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Смайлик</title><path d="M15.83 10.997a1.167 1.167 0 1 0 1.167 1.167 1.167 1.167 0 0 0-1.167-1.167Zm-6.5 1.167a1.167 1.167 0 1 0-1.166 1.167 1.167 1.167 0 0 0 1.166-1.167Zm5.163 3.24a3.406 3.406 0 0 1-4.982.007 1 1 0 1 0-1.557 1.256 5.397 5.397 0 0 0 8.09 0 1 1 0 0 0-1.55-1.263ZM12 .503a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12 .503Zm0 21a9.5 9.5 0 1 1 9.5-9.5 9.51 9.51 0 0 1-9.5 9.5Z"></path></svg>
+
                                 </div>
                                 <div>
-                                    <textarea style={{'width':'260px', 'height':'20px', 'border':'none', 'resize':'none'}} placeholder='Добавьте комментарий'/>
+                                    <textarea
+                                    value={newComment}
+                                    onChange={handleCommentChange}
+                                    style={{'width':'260px', 'height':'20px', 'border':'none', 'resize':'none'}} placeholder='Добавьте комментарий'/>
                                 </div>
                                 <div className='modal-post-section-end'>
-                                    <a href="#">Опубликовать</a>
+                                    <a href="#" style={{'colot':'#77a7ff'} }  onClick={handlePublishClick}><b>Опубликовать</b></a>
                                 </div>
                             </div>
                         </section>
                     </div>
+                         
+                </div>
+                           )
+                           )
+                                }
+                        
             </div>
+        
     );
 
 }
